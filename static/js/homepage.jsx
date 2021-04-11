24058fa7b984f6f4b281c7039580bbc59103839f
Fatima <fatima.hammouri@gmail.com>
2021-04-11 10:40:12 +0000

    1.added content to homepage.jsx 2.Modified tradingCard.jsx

---
+++ static/js/homepage.jsx
@@ -0,0 +1,13 @@
+"use strict";
+
+function Homepage() {
+  return (
+    <div>
+      <h1> Welocome </h1>
+      <a href="/cards">Click here to view the trading cards.</a>
+      <img src="/static/img/balloonicorn.jpg" />
+    </div>
+  );
+}
+
+ReactDOM.render(<Homepage />, document.querySelector('#app'));
---
+++ static/js/tradingCard.jsx
@@ -0,0 +1,99 @@
+"use strict";
+
+const tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
+  }
+];
+
+// define TradingCard component
+   // render one trading card
+
+   function TradingCard(props) {
+    return (
+      <div className="card">
+        <h2>Name: {props.name}</h2>
+        <img src={props.imgUrl} />
+        <h2>Skill: {props.skill} </h2>
+      </div>
+    );
+  }
+
+// define TradingCardContainer component
+
+  // loop over each data point
+  // and make a TradingCard component
+  // return all those TradingCard's we made
+  function TradingCardContainer() {
+    const tradingCards = [];
+
+    for (const currentCard of tradingCardData) {
+      tradingCards.push(
+      <TradingCard name={currentCard.name} 
+      imgUrl={currentCard.imgUrl} 
+      skill={currentCard.skill}/>
+      );
+    }
+
+    return (
+      <React.Fragment> {tradingCards} </React.Fragment>
+    );
+
+  }
+
+// mount just ONE thing onto the page:
+// one TradingCardContainer component
+ReactDOM.render(
+    (
+      <TradingCardContainer/>
+    ),
+    document.querySelector('#cardblock')
+  );
+
+
+console.log("hiii");
