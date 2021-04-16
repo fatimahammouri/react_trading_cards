7411e04c3638374715480aba6822f180384c64cf
Fatima <fatima.hammouri@gmail.com>
2021-04-16 03:31:16 +0000

    completed further study

---
+++ static/js/about.jsx
@@ -0,0 +1,12 @@
+"use strict";
+
+function About() {
+  return (
+    <div>
+      <h1> About the authors </h1>
+      <p> Fatima and Hannah are the best coders ever.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<About />, document.querySelector('#about'));
